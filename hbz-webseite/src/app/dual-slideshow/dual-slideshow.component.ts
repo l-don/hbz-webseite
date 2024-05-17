158523ae0abf768203110eef1281814694104b47
l-don <louis.donath@gmail.com>
2024-05-17 14:56:18 +0200

    also fetch text below images from json

--- hbz-webseite/src/app/dual-slideshow/dual-slideshow.component.ts
+++ hbz-webseite/src/app/dual-slideshow/dual-slideshow.component.ts
@@ -1,6 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {SlideshowComponent} from "../slideshow/slideshow.component";
-
+import { Component, Input, OnInit } from '@angular/core';
+import { SlideshowComponent } from "../slideshow/slideshow.component";
 
 @Component({
   selector: 'app-dual-slideshow',
@@ -9,26 +8,34 @@ import {SlideshowComponent} from "../slideshow/slideshow.component";
   standalone: true,
   imports: [SlideshowComponent]
 })
-
-
 export class DualSlideshowComponent implements OnInit {
   @Input() folder1: string = '';
   @Input() folder2: string = '';
   images1: string[] = [];
   images2: string[] = [];
+  text1: string = '';
+  text2: string = '';
 
   ngOnInit() {
-    this.loadImages();
+    this.loadContent();
   }
 
-  async loadImages() {
-    this.images1 = await this.getImagesFromFolder(this.folder1);
-    this.images2 = await this.getImagesFromFolder(this.folder2);
+  async loadContent() {
+    const content1 = await this.getContentFromFolder(this.folder1);
+    this.images1 = content1.images;
+    this.text1 = content1.text;
+
+    const content2 = await this.getContentFromFolder(this.folder2);
+    this.images2 = content2.images;
+    this.text2 = content2.text;
   }
 
-  async getImagesFromFolder(folder: string): Promise<string[]> {
+  async getContentFromFolder(folder: string): Promise<{ images: string[], text: string }> {
     const response = await fetch(`${folder}/images.json`);
     const data = await response.json();
-    return data.images.map((image: string) => `${folder}/${image}`);
+    const images = data.images.map((image: string) => `${folder}/${image}`);
+    const text = data.text;
+    return { images, text };
   }
 }
+
