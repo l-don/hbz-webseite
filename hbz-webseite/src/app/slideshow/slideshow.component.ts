e8998dd6bf26c903d4a7eb22b7ae50c2869c2f42
l-don <louis.donath@gmail.com>
2024-06-18 12:27:03 +0200

    revert adding placeholder on image load

--- hbz-webseite/src/app/slideshow/slideshow.component.ts
+++ hbz-webseite/src/app/slideshow/slideshow.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { NgIf } from "@angular/common";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-slideshow',
@@ -13,7 +13,6 @@ import { NgIf } from "@angular/common";
 export class SlideshowComponent {
   @Input() images: string[] = [];
   currentIndex: number = 0;
-  currentSlideLoaded: boolean = false;
 
   get currentSlide(): string {
     return this.images[this.currentIndex];
@@ -21,21 +20,9 @@ export class SlideshowComponent {
 
   nextSlide(): void {
     this.currentIndex = (this.currentIndex + 1) % this.images.length;
-    this.currentSlideLoaded = false;
   }
 
   prevSlide(): void {
     this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
-    this.currentSlideLoaded = false;
-  }
-
-  onImageLoad(): void {
-    this.currentSlideLoaded = true;
-  }
-
-  onImageError(): void {
-    // Handle error case if the image fails to load
-    console.error("Error loading image:", this.currentSlide);
-    this.currentSlideLoaded = false;
   }
 }
