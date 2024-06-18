02558473d71bea5a10c740463e013e8885d51256
l-don <louis.donath@gmail.com>
2024-06-18 11:44:41 +0200

    preload images in slideshows

--- hbz-webseite/src/app/dual-slideshow/dual-slideshow.component.ts
+++ hbz-webseite/src/app/dual-slideshow/dual-slideshow.component.ts
@@ -20,15 +20,26 @@ export class DualSlideshowComponent implements OnInit {
     this.loadContent();
   }
 
+  async preloadImages(images: string[]): Promise<void> {
+    for (const image of images) {
+      const img = new Image();
+      img.src = image;
+      await img.decode(); // Warte, bis das Bild geladen ist
+    }
+  }
+
   async loadContent() {
     const content1 = await this.getContentFromFolder(this.folder1);
     this.images1 = content1.images;
     this.text1 = content1.text;
+    await this.preloadImages(this.images1);
 
     const content2 = await this.getContentFromFolder(this.folder2);
     this.images2 = content2.images;
     this.text2 = content2.text;
+    await this.preloadImages(this.images2);
   }
+
 
   async getContentFromFolder(folder: string): Promise<{ images: string[], text: string }> {
     const response = await fetch(`${folder}/images.json`);
