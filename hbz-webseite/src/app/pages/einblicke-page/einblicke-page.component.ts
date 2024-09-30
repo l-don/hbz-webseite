f6f8b8d4c0d6404231e0385e852124143802146e
l-don <louis.donath@gmail.com>
2024-09-30 16:36:40 +0200

    extract neusiedler,einblicke from tross + preise

--- hbz-webseite/src/app/pages/einblicke-page/einblicke-page.component.ts
+++ hbz-webseite/src/app/pages/einblicke-page/einblicke-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {BannerImgComponent} from "../../banner-img/banner-img.component";
+import {DualSlideshowComponent} from "../../dual-slideshow/dual-slideshow.component";
 
 @Component({
   selector: 'app-einblicke-page',
   standalone: true,
-    imports: [
-        BannerImgComponent
-    ],
+  imports: [
+    BannerImgComponent,
+    DualSlideshowComponent
+  ],
   templateUrl: './einblicke-page.component.html',
   styleUrl: './einblicke-page.component.scss'
 })
