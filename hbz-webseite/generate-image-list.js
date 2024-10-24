8800e501ef4de640c79076cc18e7bd2f760ccf27
l-don <louis.donath@gmail.com>
2024-10-24 11:05:02 +0200

    year specific images and buttons for gallery

--- hbz-webseite/generate-image-list.js
+++ hbz-webseite/generate-image-list.js
@@ -1,20 +1,34 @@
 const fs = require('fs');
 const path = require('path');
 
-const directoryPath = path.join(__dirname, 'src/assets/gallery');
+const galleryPath = path.join(__dirname, 'src/assets/gallery');
 const outputPath = path.join(__dirname, 'src/assets/image-list.json');
 
-fs.readdir(directoryPath, (err, files) => {
+let years = {};
+
+fs.readdir(galleryPath, (err, yearDirs) => {
   if (err) {
     return console.log('Fehler beim Lesen des Verzeichnisses: ' + err);
   }
 
-  const images = files.filter(file => {
-    const ext = path.extname(file).toLowerCase();
-    return ['.jpg', '.jpeg', '.png', '.gif'].includes(ext);
-  }).map(file => `/assets/gallery/${file}`);
+  // Nur Verzeichnisse der Jahre filtern
+  yearDirs = yearDirs.filter(yearDir => {
+    return fs.statSync(path.join(galleryPath, yearDir)).isDirectory();
+  });
 
-  fs.writeFile(outputPath, JSON.stringify(images), err => {
+  yearDirs.forEach(yearDir => {
+    const yearPath = path.join(galleryPath, yearDir);
+    const files = fs.readdirSync(yearPath);
+
+    const images = files.filter(file => {
+      const ext = path.extname(file).toLowerCase();
+      return ['.jpg', '.jpeg', '.png', '.gif'].includes(ext);
+    }).map(file => `assets/gallery/${yearDir}/${file}`);
+
+    years[yearDir] = images;
+  });
+
+  fs.writeFile(outputPath, JSON.stringify(years), err => {
     if (err) {
       return console.log('Fehler beim Schreiben der Datei: ' + err);
     }
--- hbz-webseite/src/app/gallery-lightbox/gallery-lightbox.component.ts
+++ hbz-webseite/src/app/gallery-lightbox/gallery-lightbox.component.ts
@@ -14,16 +14,32 @@ import {CommonModule, NgForOf} from "@angular/common";
 })
 export class GalleryLightboxComponent implements OnInit {
   public albums: Array<any> = [];
+  public years: string[] = [];
+  public selectedYear: string = '';
+  private imagesByYear: { [year: string]: string[] } = {};
 
   constructor(private _lightbox: Lightbox, private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get<string[]>('/assets/image-list.json').subscribe(images => {
-      this.albums = images.map(image => ({
-        src: image,
-        thumb: image
-      }));
+    this.http.get<{ [year: string]: string[] }>('assets/image-list.json').subscribe(data => {
+      this.imagesByYear = data;
+      this.years = Object.keys(data).sort((a, b) => Number(b) - Number(a)); // Jahre sortieren
+      this.selectedYear = this.years[0]; // Standardmäßig das neueste Jahr auswählen
+      this.updateGallery();
     });
+  }
+
+  updateGallery(): void {
+    const images = this.imagesByYear[this.selectedYear] || [];
+    this.albums = images.map(image => ({
+      src: image,
+      thumb: image
+    }));
+  }
+
+  onYearChange(year: string): void {
+    this.selectedYear = year;
+    this.updateGallery();
   }
 
   open(index: number): void {
