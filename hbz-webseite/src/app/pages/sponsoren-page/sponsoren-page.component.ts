60b91f672b661d3b5cb998240eaa37e98bfd221e
l-don <louis.donath@gmail.com>
2024-11-01 13:21:26 +0100

    add sponsors

--- hbz-webseite/src/app/pages/sponsoren-page/sponsoren-page.component.ts
+++ hbz-webseite/src/app/pages/sponsoren-page/sponsoren-page.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import {BannerImgComponent} from "../../banner-img/banner-img.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-sponsoren-page',
   standalone: true,
-    imports: [
-        BannerImgComponent
-    ],
+  imports: [
+    BannerImgComponent,
+    NgForOf
+  ],
   templateUrl: './sponsoren-page.component.html',
   styleUrl: './sponsoren-page.component.scss'
 })
 export class SponsorenPageComponent {
-
+  sponsors = [
+    { name: 'Balzuhn', logo: 'balzuhn-frankenberg-logo.jpg' },
+    { name: 'BHS Transporte', logo: 'BHS Transporte Logo_Neu.jpeg' },
+    { name: 'Agraset', logo: 'Logo-Agraset.jpg' },
+    { name: 'Narrateau', logo: 'Narrateua 2.jpg' },
+    { name: 'Orgis', logo: 'Orgis Logo 2.jpg' },
+    { name: 'Sparkasse', logo: 'SPKMSN_rot.jpg' }
+  ];
 }
