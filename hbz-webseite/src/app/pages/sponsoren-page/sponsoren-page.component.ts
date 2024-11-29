004600b4b8e0fe9963db4dc6acf4aa9f4765ebe4
l-don <louis.donath@gmail.com>
2024-11-29 11:05:50 +0100

    realize feedback proposed changes

--- hbz-webseite/src/app/pages/sponsoren-page/sponsoren-page.component.ts
+++ hbz-webseite/src/app/pages/sponsoren-page/sponsoren-page.component.ts
@@ -18,7 +18,6 @@ export class SponsorenPageComponent {
   sponsors = [
     { name: 'Uwe Balzuhn e.K.', logo: 'balzuhn-frankenberg-logo.jpg', link:'https://balzuhn.de' },
     { name: 'BHS Transporte', logo: 'BHS Transporte Logo_Neu.jpeg', link:'https://www.bhs-transporte.de'  },
-    { name: 'Agraset-Agrargenossenschaft eG Naundorf bei Rochlitz', logo: 'Logo-Agraset.jpg', link:'https://www.agraset.de'  },
     { name: 'Narrateau', logo: 'Narrateua 2.jpg' , link:'https://www.narrateau.de' },
     { name: 'Orgis Werbung', logo: 'Orgis Logo 2.jpg' , link:'https://www.orgiswerbung.de' },
     { name: 'DELPHIN - Projekte gGmbH', logo: 'Delphin Logo mit Adresse.jpg' , link:'https://karree49.de/karree49' },
